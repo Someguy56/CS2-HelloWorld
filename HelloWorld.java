df004bc1ff30c7607a4c226aaf3fc4e30d7603de
Someguy56 <maxwelllee12345@yahoo.com>
2015-05-06 08:59:03 -0700

    Added Scanner
    Y

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld
 {
+  private static Scanner in = new Scanner(System.in);
+
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
     System.out.println("( ͡° ͜ʖ ͡°)");
-    System.out.println("Goodbye");
+    System.out.print("What's your name? > ");
+    String name = in.nextLine();
+    System.out.println("Goodbye, " + name + "!");
   }
 }
